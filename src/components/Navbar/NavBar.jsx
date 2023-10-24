066c8d2cf788da70c30375dce8f107092765ccca
FerreEcommerce <sebasnsd13@gmail.com>
2023-10-23 23:24:57 -0300

    ferre

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -11,13 +11,13 @@ function NavBar() {
     <h1>FERRETERIA EMILIO E-COMMERCE</h1>
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">INICIO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#home">ACERCA DE</Nav.Link>
+            <Nav.Link href="#link">CONTACTO</Nav.Link>
+            <NavDropdown title="PRODUCTOS" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
